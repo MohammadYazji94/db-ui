f3e7cf907baa33222802c43b178398aee696dd04
DirtyFileReader <35199562+MohammadYazji94@users.noreply.github.com>
2023-07-12 12:11:42 +0200

    Dauer wird berechnet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'DB-UI';
 
   trips = [
-    { from: "Frankfurt Hbf", to: "Berlin Hbf", starttime: "09:40", endtime: "11:20" },
-    { from: "Berlin Hbf", to: "Frankfurt Hbf", starttime: "10:40", endtime: "12:20" }
+    {
+      from: 'Frankfurt Hbf',
+      to: 'Berlin Hbf',
+      starttime: '09:40',
+      endtime: '11:20',
+    },
+    {
+      from: 'Berlin Hbf',
+      to: 'Frankfurt Hbf',
+      starttime: '10:40',
+      endtime: '12:20',
+    },
   ];
 
   filteredTrips = this.trips;
 
   filterTrips(city: string, direction: 'from' | 'to') {
-    this.filteredTrips = this.trips.filter(trip => trip[direction] === city);
-}
+    this.filteredTrips = this.trips.filter((trip) => trip[direction] === city);
+  }
 }
